1361cc22c5b671a682683bdaa12eb844c8c9f153
Raphaël Martin <96866454+Raph52@users.noreply.github.com>
2022-04-14 10:01:33 +0200

    Delete test_bdd.js
    !

--- test_bdd.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb+srv://Raphael:QpkKRbAjZ36JD8wi@cluster0.sszvr.mongodb.net/immo?retryWrites=true&w=majority', 
-    {connectTimeoutMS : 3000, socketTimeoutMS: 20000, useNewUrlParser: true, useUnifiedTopology: true }
-);
-const db = mongoose.connection;
-db.once('open', () => {
-   console.log(`connexion OK !`);
-});
